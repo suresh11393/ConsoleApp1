a2f0b329b0438036baa92438b347c616dfed743a
suresh11393 <34719375+suresh11393@users.noreply.github.com>
2020-11-25 12:40:11 +0530

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            processDirectory(@"D:\Products\SaralTDS12\SaralTDS\SaralTDS\Main\bin\Debug\Data\Userfiles\FY_2020_21\TDS");
+            Console.ReadLine();
+        }
+        private static void processDirectory(string startLocation)
+        {
+            foreach (var directory in Directory.GetDirectories(startLocation))
+            {
+                processDirectory(directory);
+                if (Directory.GetFiles(directory).Length == 0 &&
+                    Directory.GetDirectories(directory).Length == 0)
+                {
+                    Directory.Delete(directory, false);
+                }
+            }
+        }
+    }
+}
